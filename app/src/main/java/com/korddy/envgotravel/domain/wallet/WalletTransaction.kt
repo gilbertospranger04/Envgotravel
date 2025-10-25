846d69b54ae316d03e28cc4489a023370137c5d3
Gilberto Spranger <mauvespranger01@gmail.com>
2025-10-25 05:02:42 +0100

    Update WalletTransaction.kt

--- app/src/main/java/com/korddy/envgotravel/domain/wallet/WalletTransaction.kt
+++ app/src/main/java/com/korddy/envgotravel/domain/wallet/WalletTransaction.kt
@@ -2,13 +2,15 @@ package com.korddy.envgotravel.domain.wallet
 
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Serializable
 data class WalletTransaction(
-    val id: Int,
+    val id: UUID,
     val userId: Int,
     val amount: Double,
     val transactionBy: String,
     val description: String,
     val createdAt: LocalDateTime? = null
 )
+
