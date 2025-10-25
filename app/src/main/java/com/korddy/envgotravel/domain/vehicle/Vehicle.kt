33e422d9535719d32f07b408f26a06e5b8e7e6fd
Gilberto Spranger <mauvespranger01@gmail.com>
2025-10-25 05:03:37 +0100

    Update Vehicle.kt

--- app/src/main/java/com/korddy/envgotravel/domain/vehicle/Vehicle.kt
+++ app/src/main/java/com/korddy/envgotravel/domain/vehicle/Vehicle.kt
@@ -2,12 +2,14 @@ package com.korddy.envgotravel.domain.vehicle
 
 import com.google.gson.annotations.SerializedName
 import com.korddy.envgotravel.domain.user.User
+import java.util.UUID
 
 data class Vehicle(
-    val id: Int,
+    val id: UUID,
     val driver: User,
     val brand: String,
     val model: String,
     @SerializedName("plate_number") val plateNumber: String,
     val color: String
 )
+
