3170ca33398d3ff8b17b1f012e359b6db532de12
Gilberto Spranger <mauvespranger01@gmail.com>
2025-10-25 05:09:02 +0100

    Update DriverLocation.kt

--- app/src/main/java/com/korddy/envgotravel/domain/driver/DriverLocation.kt
+++ app/src/main/java/com/korddy/envgotravel/domain/driver/DriverLocation.kt
@@ -2,11 +2,12 @@ package com.korddy.envgotravel.domain.driver
 
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Serializable
 data class DriverLocation(
-    val id: Int,
-    val driverId: Int,
+    val id: UUID,
+    val driverId: UUID,
     val intCoord: Double,   // correspondendo ao int (float8) da imagem, renomeei para intCoord para clareza
     val reqCoord: Double,   // correspondendo ao req (float8)
     val updatedAt: LocalDateTime? = null
