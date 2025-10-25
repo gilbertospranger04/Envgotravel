6279886a568820d75f77ceada80fdf1967cebb32
Gilberto Spranger <mauvespranger01@gmail.com>
2025-10-25 05:08:03 +0100

    Update Payment.kt

--- app/src/main/java/com/korddy/envgotravel/domain/payment/Payment.kt
+++ app/src/main/java/com/korddy/envgotravel/domain/payment/Payment.kt
@@ -3,9 +3,10 @@ package com.korddy.envgotravel.domain.payment
 import com.google.gson.annotations.SerializedName
 import com.korddy.envgotravel.domain.ride.Ride
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class Payment(
-    val id: Int,
+    val id: UUID,
     val ride: Ride,
     val amount: Double,
     val status: PaymentStatus,
